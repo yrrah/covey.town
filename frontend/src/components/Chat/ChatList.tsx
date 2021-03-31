102ec362ced6b90cfdce03cd493e01472f12bc1c
PriyankaManjunath <41125566+priyankamanjunath14@users.noreply.github.com>
2021-03-31 14:38:44 -0400

    Chat UI changes Change background, color code the chats, add user ID in tool tip, add chat type for better readability.

--- frontend/src/components/Chat/ChatList.tsx
+++ frontend/src/components/Chat/ChatList.tsx
@@ -1,27 +1,65 @@
 import React from 'react';
-import { VStack, Text, Wrap, Tooltip} from "@chakra-ui/react";
-import { ChatState } from "../../CoveyTypes";
+import {Container, Text, Tooltip, VStack, Wrap} from "@chakra-ui/react";
+import {ChatState} from "../../CoveyTypes";
+import useCoveyAppState from "../../hooks/useCoveyAppState";
+
+// ToDo: Auto scroll to bottom
+export default function ChatList(props: { chatState: ChatState }): JSX.Element {
+
+  const {chatState} = props;
+  const {myPlayerID} = useCoveyAppState();
 
 
-export default function ChatList(props: { chatState: ChatState  }):JSX.Element {
+  function showChats(): string | JSX.Element[] {
 
-  const { chatState } = props;
+    if (!chatState.chats) return 'no data';
+    if (!Array.isArray(chatState.chats)) return 'results are not array'
 
-  function showChats():string | JSX.Element[]{
-      if (!chatState.chats) return 'no data';
-      if (!Array.isArray(chatState.chats)) return 'results are not array'
-      return chatState.chats.map(chat => (
-        <Wrap key={chat.timestamp.getMilliseconds()} align='left'>
-          <Tooltip label={`Sent at ${chat.timestamp.toLocaleTimeString()}`} aria-label="Message Timestamp">
-            <Text as='b'>{ chat.sendingPlayer.userName }:&nbsp;</Text>
+    function chatColor(chatType: string, sendingPlayerID: string) {
+      if (sendingPlayerID === myPlayerID) {
+        return "yellowgreen";
+      }
+      if (chatType === "public") {
+        return "powderblue"
+      }
+      if (chatType === "private") {
+        return "wheat"
+      }
+      return "tomato"
+    }
+
+    function chatUserName(sendingPlayerID: string, sendingPlayerUserName: string) {
+      if (sendingPlayerID === myPlayerID) {
+        return "Me"
+      }
+      return sendingPlayerUserName
+    }
+
+    return chatState.chats.map(chat => (
+      <Wrap key={chat.timestamp.getMilliseconds()} align='left'>
+        <Text ml={2} color='white'>({chat.chatType})</Text>
+        <Container display="flex" m={2} mt={0}
+                   backgroundColor={chatColor(chat.chatType, chat.sendingPlayer.id)}
+                   borderRadius="10px"
+                   padding="1">
+          <Tooltip label={`User ID:  ${chat.sendingPlayer.id}`}
+                   aria-label="User ID">
+            <Text
+              as='b'>{chatUserName(chat.sendingPlayer.id, chat.sendingPlayer.userName)}:&nbsp; </Text>
           </Tooltip>
-          <Text> { chat.message }</Text>
-        </Wrap>
-      ))
+          <Tooltip label={`Sent at ${chat.timestamp.toLocaleTimeString()}`}
+                   aria-label="Message Timestamp">
+            <Text display="flex"> {chat.message}</Text>
+          </Tooltip>
+
+        </Container>
+      </Wrap>
+    ))
   }
+
   return (
     <VStack>
-      { showChats() }
+      {showChats()}
     </VStack>
   );
 }
--- frontend/src/components/Chat/ChatPanel.tsx
+++ frontend/src/components/Chat/ChatPanel.tsx
@@ -1,33 +1,26 @@
 import React, {useCallback, useEffect, useState} from 'react';
-import {
-  Button,
-  Select,
-  Flex,
-  Spacer,
-  Box,
-  Input,
-} from '@chakra-ui/react';
+import {Box, Button, Flex, Input, Select, Spacer,} from '@chakra-ui/react';
 import useCoveyAppState from "../../hooks/useCoveyAppState";
 import {ChatState, ChatType, ChatUpdate, ReceivingPlayerID} from "../../CoveyTypes";
 import ChatList from "./ChatList";
 
-function ChatPanel(props: { chatState: ChatState, updateChatState: React.Dispatch<ChatUpdate> }):JSX.Element {
+function ChatPanel(props: { chatState: ChatState, updateChatState: React.Dispatch<ChatUpdate> }): JSX.Element {
   const [chatInput, setChatInput] = useState('');
   const {userName, myPlayerID, players, nearbyPlayers} = useCoveyAppState();
-  const { chatState, updateChatState } = props;
+  const {chatState, updateChatState} = props;
   const [receivingPlayerList, addReceivingPlayers] = useState<ReceivingPlayerID[]>();
   const [chatMode, setChatMode] = useState<ChatType>('public');
   const playersList = players.filter((player) => player.id !== myPlayerID);
 
   const updateNearbyPlayers = useCallback(() => {
     const proximityPlayerList: ReceivingPlayerID[] = [];
-      nearbyPlayers.nearbyPlayers.forEach(player => {
-        proximityPlayerList.push({ playerID: player.id });
-      });
-      proximityPlayerList.push({ playerID: myPlayerID });
-      addReceivingPlayers(proximityPlayerList);
+    nearbyPlayers.nearbyPlayers.forEach(player => {
+      proximityPlayerList.push({playerID: player.id});
+    });
+    proximityPlayerList.push({playerID: myPlayerID});
+    addReceivingPlayers(proximityPlayerList);
   }, [myPlayerID, nearbyPlayers.nearbyPlayers]);
-  
+
   useEffect(() => {
     updateNearbyPlayers();
   }, [updateNearbyPlayers]);
@@ -44,7 +37,7 @@ function ChatPanel(props: { chatState: ChatState, updateChatState: React.Dispatc
         }
     })
   }
-  
+
   function handleChangeList(item: string) {
     if(item === 'Proximity Chat') {
       setChatMode('proximity');
@@ -66,20 +59,21 @@ function ChatPanel(props: { chatState: ChatState, updateChatState: React.Dispatc
     }
   }
 
-  return <Flex bg='lightgrey' direction='column' height='100%'>
+
+  return <Flex bg='lightgrey' direction='column' height='600px' marginTop="50px">
     <Flex justify='flex-end'>
       <Button colorScheme="teal" variant="ghost">
         X
       </Button>
     </Flex>
-    <Box bg='white' flex={1} m={2}>
-      <ChatList chatState={chatState} />
+    <Box bg='black' flex={1} m={2} overflow="scroll" overflowY="auto">
+      <ChatList chatState={chatState}/>
     </Box>
     <Select data-testid="playerList" variant="filled" placeHolder='Everyone' onChange={item => handleChangeList(item.target.value)}>
     <option> Everyone </option>
     <option> Proximity Chat </option>
       { playersList.map(player => (
-        <option key={player.id}> {player.userName} </option> 
+        <option key={player.id}> {player.userName} </option>
         )) }
     </Select>
     <Flex direction='row'>
