c60b3dcfa873a911e38a8808a38b39e271a76533
PriyankaManjunath <41125566+priyankamanjunath14@users.noreply.github.com>
2021-04-01 16:42:08 -0400

    scroll chat window to the newest message

--- frontend/src/components/Chat/ChatPanel.tsx
+++ frontend/src/components/Chat/ChatPanel.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 import {Box, Button, Flex, Input, Select, Spacer,} from '@chakra-ui/react';
 import useCoveyAppState from "../../hooks/useCoveyAppState";
 import {ChatState, ChatType, ChatUpdate, ReceivingPlayerID} from "../../CoveyTypes";
@@ -11,7 +11,16 @@ function ChatPanel(props: { chatState: ChatState, updateChatState: React.Dispatc
   const [receivingPlayerList, addReceivingPlayers] = useState<ReceivingPlayerID[]>();
   const [chatMode, setChatMode] = useState<ChatType>('public');
   const playersList = players.filter((player) => player.id !== myPlayerID);
-  const [files, setFiles] = useState<FileList|null>(null);
+  const [files, setFiles] = useState<FileList | null>(null);
+  const myRef = useRef(null);
+
+  const executeScroll = () => {
+    const node = (myRef.current) as unknown as Element;
+    node.scrollIntoView({
+      block: 'nearest', behavior: 'smooth',
+      inline: "end"
+    });
+  };
 
   const updateNearbyPlayers = useCallback(() => {
     const proximityPlayerList: ReceivingPlayerID[] = [];
@@ -26,7 +35,11 @@ function ChatPanel(props: { chatState: ChatState, updateChatState: React.Dispatc
     updateNearbyPlayers();
   }, [updateNearbyPlayers]);
 
-  function sendMessage(message:string, fileName?:string) {
+  useEffect(() => {
+    executeScroll();
+  }, [chatState]);
+
+  function sendMessage(message: string, fileName?: string) {
     updateChatState({
       action: 'sendMessage',
       data: {
@@ -36,27 +49,25 @@ function ChatPanel(props: { chatState: ChatState, updateChatState: React.Dispatc
         sendingPlayer: {id: myPlayerID, userName},
         receivingPlayerID: receivingPlayerList,
         chatType: chatMode,
-        }
+      }
     })
   }
 
   function handleChangeList(item: string) {
-    if(item === 'Proximity Chat') {
+    if (item === 'Proximity Chat') {
       setChatMode('proximity');
       updateNearbyPlayers();
-    }
-    else if(item === 'Everyone') {
+    } else if (item === 'Everyone') {
       setChatMode('public');
       addReceivingPlayers(undefined);
-    }
-    else {
+    } else {
       setChatMode('private');
       const privatePlayerList: ReceivingPlayerID[] = [];
       const privatePlayer = players.find(player => player.userName === item);
-      if(privatePlayer) {
-        privatePlayerList.push({ playerID: privatePlayer.id });
+      if (privatePlayer) {
+        privatePlayerList.push({playerID: privatePlayer.id});
       }
-      privatePlayerList.push({ playerID: myPlayerID });
+      privatePlayerList.push({playerID: myPlayerID});
       addReceivingPlayers(privatePlayerList);
     }
   }
@@ -64,11 +75,11 @@ function ChatPanel(props: { chatState: ChatState, updateChatState: React.Dispatc
   async function doUpload() {
     if (files && files[0]) {
       const response = await apiClient.uploadFile({
-          file: files[0],
-          name: 'chatFile',
-          token:sessionToken,
-          coveyTownID:currentTownID
-        })
+        file: files[0],
+        name: 'chatFile',
+        token: sessionToken,
+        coveyTownID: currentTownID
+      })
       if (response.fileName) {
         sendMessage(response.name, response.fileName)
       }
@@ -77,28 +88,33 @@ function ChatPanel(props: { chatState: ChatState, updateChatState: React.Dispatc
 
   return <Flex bg='lightgrey' direction='column' height='600px' marginTop="50px">
     <Flex justify='flex-end'>
-      <Input type='file' name='chatFile' onChange={(event) => {setFiles(event.target.files)}}/>
-      <Button data-testid="uploadFile" onClick={ () => doUpload() } color="blue">Upload</Button>
+      <Input type='file' name='chatFile' onChange={(event) => {
+        setFiles(event.target.files)
+      }}/>
+      <Button data-testid="uploadFile" onClick={() => doUpload()} color="blue">Upload</Button>
       <Button colorScheme="teal" variant="ghost">
         X
       </Button>
     </Flex>
     <Box bg='black' flex={1} m={2} overflow="scroll" overflowY="auto">
       <ChatList chatState={chatState}/>
+      <div ref={myRef}/>
     </Box>
-    <Select data-testid="playerList" variant="filled" placeHolder='Everyone' onChange={item => handleChangeList(item.target.value)}>
-    <option> Everyone </option>
-    <option> Proximity Chat </option>
-      { playersList.map(player => (
+    <Select data-testid="playerList" variant="filled" placeHolder='Everyone'
+            onChange={item => handleChangeList(item.target.value)}>
+      <option> Everyone</option>
+      <option> Proximity Chat</option>
+      {playersList.map(player => (
         <option key={player.id}> {player.userName} </option>
-        )) }
+      ))}
     </Select>
     <Flex direction='row'>
       <Input data-testid="chatInput" placeholder="Chat input"
              value={chatInput}
-             onChange={event => setChatInput(event.target.value)}  />
-      <Spacer flex={1} />
-      <Button data-testid="sendChat" onClick={ () => sendMessage(chatInput) } color="blue">Send</Button>
+             onChange={event => setChatInput(event.target.value)}/>
+      <Spacer flex={1}/>
+      <Button data-testid="sendChat" onClick={() => sendMessage(chatInput)}
+              color="blue">Send</Button>
     </Flex>
   </Flex>
 }
