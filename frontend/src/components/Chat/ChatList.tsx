7ddb8b4e8a0a609fa79a0300c3dd61b77e75f149
PriyankaManjunath <41125566+priyankamanjunath14@users.noreply.github.com>
2021-04-06 16:54:39 -0400

    Align chat bubbles to left and right

--- frontend/src/components/Chat/ChatList.tsx
+++ frontend/src/components/Chat/ChatList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container, Text, Tooltip, VStack, Wrap, Link} from "@chakra-ui/react";
+import {Box, Container, Link, Text, Tooltip, VStack} from "@chakra-ui/react";
 import {ChatData, ChatState} from "../../CoveyTypes";
 import useCoveyAppState from "../../hooks/useCoveyAppState";
 
@@ -11,7 +11,7 @@ export default function ChatList(props: { chatState: ChatState }): JSX.Element {
   function showChats(): string | JSX.Element[] {
 
     if (!chatState.chats) return 'no data';
-    if (!Array.isArray(chatState.chats)) return 'results are not array'
+    if (!Array.isArray(chatState.chats)) return 'results are not array';
 
     function chatColor(chatType: string, sendingPlayerID: string) {
       if (sendingPlayerID === myPlayerID) {
@@ -33,7 +33,7 @@ export default function ChatList(props: { chatState: ChatState }): JSX.Element {
       }
       const playerNameText = (<Text as='b'> {sender}:&nbsp; </Text>);
       // hide player id of server messages
-      if (sendingPlayerID === '0'){
+      if (sendingPlayerID === '0') {
         return playerNameText
       }
       return (
@@ -43,40 +43,43 @@ export default function ChatList(props: { chatState: ChatState }): JSX.Element {
       );
     }
 
-    function chatMessage(chat:ChatData){
-      if(chat.fileName){
+    function chatMessage(chat: ChatData) {
+      if (chat.fileName) {
         return (
           <Link display="flex"
-            href={`${process.env.REACT_APP_TOWNS_SERVICE_URL}/files/${chat.fileName}`}
-            download={chat.message} target="_blank">
-            { chat.message }
+                href={`${process.env.REACT_APP_TOWNS_SERVICE_URL}/files/${chat.fileName}`}
+                download={chat.message} target="_blank">
+            {chat.message}
           </Link>)
       }
-      return (<Text style={{whiteSpace:"pre"}} display="flex"> {chat.message} </Text>)
+      return (<Text style={{whiteSpace: "pre"}} display="flex"> {chat.message} </Text>)
     }
 
     return chatState.chats.map(chat => (
-      <Wrap key={chat.timestamp.getMilliseconds()} align='left'>{
-        chat.chatType === 'proximity' &&
-        <Text ml={2} color='powderblue' fontSize="sm">(Proximity Chat)</Text>
-      }
-      {chat.chatType === 'private' &&
-        <Text ml={2} color='tomato' fontSize="sm">(Private Chat)</Text>
-      }
-        <Container display="flex" m={2} mt={0}
-                   backgroundColor={chatColor(chat.chatType, chat.sendingPlayer.id)}
-                   borderRadius="10px"
-                   padding="1">
+      <VStack key={chat.timestamp.getMilliseconds()}
+              alignSelf={chat.sendingPlayer.id === myPlayerID ? "end" : "baseline"} mr={3} ml={3}>
+        <Box>{
+          chat.chatType === 'proximity' &&
+          <Text ml={2} color='powderblue' fontSize="sm">(Proximity Chat)</Text>
+        }
+          {chat.chatType === 'private' &&
+          <Text ml={2} color='tomato' fontSize="sm">(Private Chat)</Text>
+          }
+          <Container display="flex" m={2} mt={0}
+                     backgroundColor={chatColor(chat.chatType, chat.sendingPlayer.id)}
+                     borderRadius="10px"
+                     padding="1">
 
             {chatUserName(chat.sendingPlayer.id, chat.sendingPlayer.userName)}
 
-          <Tooltip label={`Sent at ${chat.timestamp.toLocaleTimeString()}`}
-                   aria-label="Message Timestamp">
-            {chatMessage(chat)}
-          </Tooltip>
+            <Tooltip label={`Sent at ${chat.timestamp.toLocaleTimeString()}`}
+                     aria-label="Message Timestamp">
+              {chatMessage(chat)}
+            </Tooltip>
 
-        </Container>
-      </Wrap>
+          </Container>
+        </Box>
+      </VStack>
     ))
   }
 
